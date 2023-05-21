788633981c4725f76288501393eae804918c7395
workd <you@example.com>
2023-05-22 01:19:31 +0700

    Add project files.

---
+++ Demo_Basic/Program.cs
@@ -0,0 +1,12 @@
+﻿int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue, secondValue);
+
+List<int> result = new List<int>
+{
+    1,2,3,4,5
+};
+
+System.Collections.ArrayList.ReadOnly
+Math.Max(result);
+Console.WriteLine(largerValue);
